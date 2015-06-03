9f23b7894fdce58e77cce903abdab5d463668483
drivfe <ivanfelicianox@gmail.com>
2015-06-03 15:41:32 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "pypcpp",
+    version = "0.1",
+    author = "drivfe",
+    description = (
+		"Unofficial command line interface for https://pcpartpicker.com/"
+	),
+    keywords = "pcpartpicker",
+    url = "https://github.com/drivfe/pypcpp",
+    packages=['pypcpp'],
+    long_description=read('README.md')
+)
