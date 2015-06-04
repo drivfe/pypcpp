11efb2604afaba27716d2f96daf5f76c2cab98be
drivfe <ivanfelicianox@gmail.com>
2015-06-04 00:32:49 -0400

    whitespace setuppy fix

--- setup.py
+++ setup.py
@@ -2,22 +2,22 @@ import os
 from setuptools import setup
 
 def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+	return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name = "pypcpp",
-    version = "0.1",
-    author = "drivfe",
-    description = (
+	name = "pypcpp",
+	version = "0.1",
+	author = "drivfe",
+	description = (
 		"Unofficial command line interface for https://pcpartpicker.com/"
 	),
-    keywords = "pcpartpicker",
-    url = "https://github.com/drivfe/pypcpp",
-    packages=['pypcpp'],
+	keywords = "pcpartpicker",
+	url = "https://github.com/drivfe/pypcpp",
+	packages=['pypcpp'],
 	install_requires=[
 		"beautifulsoup4",
 		"requests",
 		"PrettyTable"
 	],
-    long_description=read('README.md')
+	long_description=read('README.md')
 )
